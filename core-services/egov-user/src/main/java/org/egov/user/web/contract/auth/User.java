1f00dfa2c4ca02daf15dab454dc6c57ba88b4b78
saurabh_gitlab <saurabh.pandey@in.ey.com>
2025-12-12 16:36:54 +0530

    add gender in User when login UserRequest Return response

--- core-services/egov-user/src/main/java/org/egov/user/web/contract/auth/User.java
+++ core-services/egov-user/src/main/java/org/egov/user/web/contract/auth/User.java
@@ -18,6 +18,8 @@ public class User implements Serializable {
     private String userName;
     private String name;
     private String mobileNumber;
+    //Add Gender
+    private String gender;
     private String emailId;
     private String locale;
     private String type;
